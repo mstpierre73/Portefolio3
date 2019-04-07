24cfb31c8511801a2d31a80510ed41d4d5599c29
mstpierre73 <naranjaqc@hotmail.com>
2019-04-06 20:02:55 -0400

    trying to fix url toggle in portefolio2

--- portefolio2/script2/main2.js
+++ portefolio2/script2/main2.js
@@ -3,26 +3,41 @@ $(document).ready(function (){
 	$('.header').css("background-image","url('img2/neige.jpg')");
 	$('.main').css("background-image","url('img2/foret.jpg')");
 	$('.aside').css("background-image","url('img2/poste.jpg')");
+	var header = $(".header").getAttribute("url");
 });
 });
-
 
 /*
-var surprise = document.getElementById("surprise").style.backgroundImage;
-var header = document.getElementById("surprise").style.backgroundImage = "url('img2/coquillages.jpg')";
-var header2 = document.getElementById("surprise").style.backgroundImage = "url('img2/neige.jpg')";
-var button = document.getElementById("change");
 
-button.onclick = function(){
-	if(surprise == header){
-		surprise = header2;
-	} else {
-		surprise = .header;
-	}
+$(document).ready(function(){
+	$('#change').click(function(){
+		var coquillage = "url('img2/coquillages.jpg')";
+		var vagues = $(".main").css("background-image");
+		var plage = $(".aside").css("background-image");
 
-	}
+		if($("#surprise").css("background-image") == coquillage){
+			$('.header').css("background-image","url('img2/neige.jpg')");
+			$('.main').css("background-image","url('img2/foret.jpg')");
+			$('.aside').css("background-image","url('img2/poste.jpg')");
+		}
+
+		else {
+			$('.header').css("background-image","url('img2/coquillages.jpg')");
+			$('.main').css("background-image","url('img2/vagues.jpg')");
+			$('.aside').css("background-image","url('img2/bouteille.jpg')");}
+		});
+	});
 
 */
+
+
+
+
+
+
+
+
+
 
 
 
