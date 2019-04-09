452ad39c9c7a925cab014ff3d4717527e5973921
mstpierre73 <naranjaqc@hotmail.com>
2019-04-09 19:59:08 -0400

    adding a new js file to portefolio2

--- portefolio2/script2/main2.js
+++ portefolio2/script2/main2.js
@@ -7,28 +7,9 @@ $(document).ready(function (){
 });
 });
 
-/*
 
-$(document).ready(function(){
-	$('#change').click(function(){
-		var coquillage = "url('img2/coquillages.jpg')";
-		var vagues = $(".main").css("background-image");
-		var plage = $(".aside").css("background-image");
 
-		if($("#surprise").css("background-image") == coquillage){
-			$('.header').css("background-image","url('img2/neige.jpg')");
-			$('.main').css("background-image","url('img2/foret.jpg')");
-			$('.aside').css("background-image","url('img2/poste.jpg')");
-		}
 
-		else {
-			$('.header').css("background-image","url('img2/coquillages.jpg')");
-			$('.main').css("background-image","url('img2/vagues.jpg')");
-			$('.aside').css("background-image","url('img2/bouteille.jpg')");}
-		});
-	});
-
-*/
 
 
 
