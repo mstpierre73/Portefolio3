112d22534ecf7b56a08ea169cb4549830f55fdc8
mstpierre73 <naranjaqc@hotmail.com>
2019-04-22 10:37:36 -0400

    adding style and finishing js script

--- todo-app/assets-todo/js-todo/todo.js
+++ todo-app/assets-todo/js-todo/todo.js
@@ -1,12 +1,21 @@
 //check off todos when they are done
-$("li").click(function() {
+$("ul").on("click", "li", function() {
 	$(this).toggleClass("completed");
 });
 
 //Delete todos when clicking on trash can
-$("span").click(function(event){
+$("ul").on("click", "span", function(event){
 	$(this).parent().fadeOut(500, function(){
 		$(this).remove();
 	});
 	event.stopPropagation();
+});
+
+//adding new todos to the list
+$("input[type='text']").keypress(function(event){
+	if(event.which === 13){
+		let todoText = $(this).val();
+		$(this).val("");
+		$("ul").append("<li><span>X</span> " + todoText + "</li>");
+	}
 });
